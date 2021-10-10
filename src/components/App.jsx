912f8775bb5c0a2c6278c4dd0ccfb52a95b42af3
Marlon Lavanda <marlonlavanda@hotmail.com>
2021-10-10 13:36:57 +0100

    configuration of the project

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/global.scss'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World Bienbenidos al curso práctico de React</h1>
+    </div>
+  )
+}
+
+export default App
